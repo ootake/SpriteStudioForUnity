beb9bf491f64f68ab740fe41355dcd5b6b6b00b8
otake <ootake1@gmail.com>
2015-10-04 13:29:40 +0900

    Update コード整備

--- SpriteStudioForUnity/Assets/SpriteStudioForUnity/Scripts/SpriteStudioPart.cs
+++ SpriteStudioForUnity/Assets/SpriteStudioForUnity/Scripts/SpriteStudioPart.cs
@@ -8,9 +8,14 @@ namespace SpriteStudioForUnity
 	[ExecuteInEditMode]
 	public class SpriteStudioPart : MonoBehaviour
 	{
+        [HideInInspector]
 		public int arrayIndex;
-		public int parentIndex;
-		public PartType partType;
+        [HideInInspector]
+        public int parentIndex;
+        [HideInInspector]
+        public SpriteStudioPart parent;
+
+        public PartType partType;
 		public PartBoundsType boundsType;
 		public PartAlphaBlendType alphaBlendType;
 		public PartInheritType inheritType;
@@ -21,7 +26,6 @@ namespace SpriteStudioForUnity
 		public bool inheritIflh;
 		public bool inheritIflv;
 		public string path;
-		public SpriteStudioPart parent;
 		public float alpha = 1;
 		float _alpha;
 		public float sortingOrder;
@@ -71,8 +75,10 @@ namespace SpriteStudioForUnity
 
 		public SpriteStudioCell cell;
 		public SpriteStudioController controller;
+
 		MeshRenderer meshRenderer;
 		MeshFilter meshFilter;
+        Mesh mesh;
 		float opacity = 1;
 		float _opacity = 1;
 
@@ -80,17 +86,14 @@ namespace SpriteStudioForUnity
 		{
 			meshRenderer = GetComponent<MeshRenderer> ();
 			meshFilter = GetComponent<MeshFilter> ();
-			meshFilter.sharedMesh = new Mesh ();
+            mesh = new Mesh();
+            mesh.MarkDynamic();
+			meshFilter.sharedMesh = mesh;
 			UpdateMesh ();
 		}
 		
 		void Update ()
 		{
-			if (cellId == -1) {
-				meshRenderer.enabled = false;
-				return;
-			}
-
 			if (rotationX != _rotationX) {
 				_rotationX = rotationX;
 				Vector3 localEulerAngles = transform.localEulerAngles;
@@ -109,15 +112,19 @@ namespace SpriteStudioForUnity
 				localEulerAngles.z = rotationZ;
 				transform.localEulerAngles = localEulerAngles;
 			}
-			if (sortingOrder != _sortingOrder) {
-				_sortingOrder = sortingOrder;
-				meshRenderer.sortingOrder = (int)sortingOrder * 10 + arrayIndex;
 
-			}
+            if (partType == PartType.Null)
+                return;
 
-		
+            if (cellId == -1) {
+                meshRenderer.enabled = false;
+                return;
+            }            
 
-
+            if (sortingOrder != _sortingOrder) {
+                _sortingOrder = sortingOrder;
+                meshRenderer.sortingOrder = (int)sortingOrder * 10 + arrayIndex;
+            }
 
 			if (inheritType == PartInheritType.Parent) {
 				SpriteStudioPart current = parent;
@@ -218,7 +225,6 @@ namespace SpriteStudioForUnity
 
 		void UpdateMesh ()
 		{
-			Mesh mesh = meshFilter.sharedMesh;
 			mesh.Clear ();
 
 			if (cell == null)
@@ -230,69 +236,63 @@ namespace SpriteStudioForUnity
 			float vR = cell.size.x * (0.5f - cell.pivot.x - offsetX) / cell.pixelPerUnit;
 			float vB = cell.size.y * (-0.5f - cell.pivot.y - offsetY) / cell.pixelPerUnit;
 			float vT = cell.size.y * (0.5f - cell.pivot.y - offsetY) / cell.pixelPerUnit;
-			Vector3 vLB = new Vector3 (vL + vertexLB.x / cell.pixelPerUnit, vB + vertexLB.y / cell.pixelPerUnit, 0.0f);
-			Vector3 vRB = new Vector3 (vR + vertexRB.x / cell.pixelPerUnit, vB + vertexRB.y / cell.pixelPerUnit, 0.0f);
-			Vector3 vLT = new Vector3 (vL + vertexLT.x / cell.pixelPerUnit, vT + vertexLT.y / cell.pixelPerUnit, 0.0f);
-			Vector3 vRT = new Vector3 (vR + vertexRT.x / cell.pixelPerUnit, vT + vertexRT.y / cell.pixelPerUnit, 0.0f);
+            Vector3 vLT = new Vector3 (vL + vertexLT.x / cell.pixelPerUnit, vT + vertexLT.y / cell.pixelPerUnit, 0.0f);
+            Vector3 vRT = new Vector3 (vR + vertexRT.x / cell.pixelPerUnit, vT + vertexRT.y / cell.pixelPerUnit, 0.0f);
+            Vector3 vRB = new Vector3 (vR + vertexRB.x / cell.pixelPerUnit, vB + vertexRB.y / cell.pixelPerUnit, 0.0f);
+            Vector3 vLB = new Vector3 (vL + vertexLB.x / cell.pixelPerUnit, vB + vertexLB.y / cell.pixelPerUnit, 0.0f);
 			Vector3 vC = Vector3.Lerp (Vector3.Lerp (vLB, vRB, 0.5f), Vector3.Lerp (vLT, vRT, 0.5f), 0.5f);
-			Vector3[] vertices = new Vector3[]
+			mesh.vertices = new Vector3[]
 			{
-				vLB,
-				vRB,
-				vLT,
-				vRT,
-				vC,
-			};
-			int[] triangles = new int[]
-			{
-				0, 1, 4,
-				1, 3, 4,
-				2, 3, 4,
-				2, 0, 4,
+                vLT,
+                vRT,
+                vRB,
+                vLB,
+                vC,
 			};
 
-			Vector2 uvLB = new Vector2 (cell.uvL, cell.uvB);
-			Vector2 uvRB = new Vector2 (cell.uvR, cell.uvB);
-			Vector2 uvLT = new Vector2 (cell.uvL, cell.uvT);
-			Vector2 uvRT = new Vector2 (cell.uvR, cell.uvT);
-			Vector2 uvC = Vector2.Lerp (uvLB, uvRT, 0.5f);
-			Vector2[] uv = null;
-			if (flipH && flipV) {
-				uv = new Vector2[]{ uvRT,uvLT,uvRB,uvLB,uvC,};
-			} else if (flipH) {
-				uv = new Vector2[]{ uvRB,uvLB,uvRT,uvLT,uvC,};
-			} else if (flipV) {
-				uv = new Vector2[]{ uvLT,uvRT,uvLB,uvRB,uvC,};
-			} else {
-				uv = new Vector2[]{ uvLB,uvRB,uvLT,uvRT,uvC,};
-			}
-			float uv2y = (float)colorBlendValue + 0.01f;
+            Vector2 uvLT = new Vector2 (cell.uvL, cell.uvT);
+            Vector2 uvRT = new Vector2 (cell.uvR, cell.uvT);
+            Vector2 uvRB = new Vector2 (cell.uvR, cell.uvB);
+            Vector2 uvLB = new Vector2 (cell.uvL, cell.uvB);
+            Vector2 uvC = Vector2.Lerp (uvLB, uvRT, 0.5f);
+            if (flipH && flipV) {
+                mesh.uv = new Vector2[]{ uvRB,uvLB,uvLT,uvRT,uvC,};
+            } else if (flipH) {
+                mesh.uv = new Vector2[]{ uvRT,uvLT,uvLB,uvRB,uvC,};
+            } else if (flipV) {
+                mesh.uv = new Vector2[]{ uvLB,uvRB,uvRT,uvLT,uvC,};
+            } else {
+                mesh.uv = new Vector2[]{ uvLT,uvRT,uvRB,uvLB,uvC,};
+            }
 
-			Vector2[] uv2 = new Vector2[]
-			{
-				new Vector2 (rateLB * opacity, uv2y),
-				new Vector2 (rateRB * opacity, uv2y),
-				new Vector2 (rateLT * opacity, uv2y),
-				new Vector2 (rateRT * opacity, uv2y),
-				new Vector2 (rateRT * opacity, uv2y),
-			};
-			Color colorC = Color.Lerp (Color.Lerp (colorLB, colorRB, 0.5f), Color.Lerp (colorLT, colorRT, 0.5f), 0.5f);
-			Color32[] colors32 = new Color32[]
-			{
-				colorLB,
-				colorRB,
-				colorLT,
-				colorRT,
-				colorC,
-			};
+            float uv2y = (float)colorBlendValue + 0.01f;
+            mesh.uv2 = new Vector2[]
+            {
+                new Vector2 (rateLT * opacity, uv2y),
+                new Vector2 (rateRT * opacity, uv2y),
+                new Vector2 (rateRB * opacity, uv2y),
+                new Vector2 (rateLB * opacity, uv2y),
+                new Vector2 (rateRT * opacity, uv2y),
+            };
 
+            mesh.triangles = new int[]
+            {
+                0, 1, 4,
+                1, 2, 4,
+                2, 3, 4,
+                3, 0, 4,
+            };
 
+            Color colorC = Color.Lerp (Color.Lerp (colorLB, colorRB, 0.5f), Color.Lerp (colorLT, colorRT, 0.5f), 0.5f);
+            mesh.colors32 = new Color32[]
+            {
+                colorLT,
+                colorRT,
+                colorRB,
+                colorLB,
+                colorC,
+            };
 
-			mesh.vertices = vertices;
-			mesh.triangles = triangles;
-			mesh.uv = uv;
-			mesh.uv2 = uv2;
-			mesh.colors32 = colors32;
 			mesh.RecalculateNormals ();
 		}
 
