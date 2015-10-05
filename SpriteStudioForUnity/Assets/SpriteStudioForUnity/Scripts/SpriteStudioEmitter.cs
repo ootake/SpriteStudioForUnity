be9671b274d9f2062b3bc3b44a4142bf22da18a6
otake <ootake1@gmail.com>
2015-10-05 11:14:38 +0900

    Update エミッターデータクラス作成

--- SpriteStudioForUnity/Assets/SpriteStudioForUnity/Scripts/SpriteStudioEmitter.cs
+++ SpriteStudioForUnity/Assets/SpriteStudioForUnity/Scripts/SpriteStudioEmitter.cs
@@ -3,8 +3,75 @@ using System.Collections;
 
 namespace SpriteStudioForUnity
 {
+    public class SpriteStudioEmitterOverWriteSeed
+    {
+        public int seed;
+    }
+
+    public class SpriteStudioEmitterDelay
+    {
+        public int delayTime;
+    }
+
+    public class SpriteStudioEmitterGravity
+    {
+        public Vector2 gravity;
+    }
+
+    public class SpriteStudioEmitterInitPosition
+    {
+        public float startOffsetX;
+        public float endOffsetX;
+        public float startOffsetY;
+        public float endOffsetY;
+    }
+
+    public class SpriteStudioEmitterTransSpeed
+    {
+        public float startSpeed;
+        public float endSpeed;
+    }
+
+    public class SpriteStudioEmitterInitRotation
+    {
+    }
+
+    public class SpriteStudioEmitterTransToration
+    {
+    }
+
+    public class SpriteStudioEmitterAddTangentiala
+    {
+    }
+
+    public class SpriteStudioEmitterAddPointGravity
+    {
+    }
+
+    public class SpriteStudioEmitterInitVertexColor
+    {
+    }
+
+    public class SpriteStudioEmitterTransVertexColor
+    {
+    }
+
+    public class SpriteStudioEmitterInitSize
+    {
+    }
+
+    public class SpriteStudioEmitterTransSize
+    {
+    }
+
+    public class SpriteStudioEmitterTransColorFade
+    {
+    }
+
+
     public class SpriteStudioEmitter : MonoBehaviour
     {
+        public int fps;
         public int priority;
         public int maximumParticle;
         public int attimeCreate;
@@ -16,9 +83,12 @@ namespace SpriteStudioForUnity
         public double angle;
         public double angleVariance;
 
+        ParticleSystem particleSystem;
+
         void Start()
         {
-            
+            particleSystem = gameObject.AddComponent<ParticleSystem>();
+
         }
 
         void Update()
