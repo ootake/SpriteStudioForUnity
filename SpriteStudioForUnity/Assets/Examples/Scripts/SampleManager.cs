6c2d5965f68cec84349da2bbd646d96d893868ef
otake <ootake1@gmail.com>
2025-04-30 06:21:18 +0900

    update: example files

--- SpriteStudioForUnity/Assets/Examples/Scripts/SampleManager.cs
+++ SpriteStudioForUnity/Assets/Examples/Scripts/SampleManager.cs
@@ -4,11 +4,6 @@ using System.Collections.Generic;
 
 
 public class SampleManager : MonoBehaviour {
-	public enum ShowType {
-		Enumeration,
-		Queue,
-	}
-	public ShowType showType = ShowType.Enumeration;
 	List<Animator> animatorList = new List<Animator>();
 	Animator currentAnimator;
 	int currentIndex = 0;
@@ -22,45 +17,25 @@ public class SampleManager : MonoBehaviour {
 			obj.SetActive(false);
 		}
 
-
-		switch (showType) {
-		case ShowType.Enumeration: {
-			for(int i = 0; i < animatorList.Count; i++){
-				GameObject obj = animatorList[i].transform.parent.gameObject;
-				obj.SetActive(true);
-				float x = -20 + (i / 5) * 5;
-				float y = 10 - (i % 5) * 5;
-				Vector3 pos = new Vector3(x, y, i);
-				obj.transform.localPosition = pos;
-			}
-			break;
+		if(animatorList.Count >= 1){
+			currentAnimator = animatorList[0];
+			GameObject obj = currentAnimator.transform.parent.gameObject;
+			obj.SetActive(true);
 		}
-		case ShowType.Queue: {
-			if(animatorList.Count >= 1){
-				currentAnimator = animatorList[0];
-				GameObject obj = currentAnimator.transform.parent.gameObject;
-				obj.SetActive(true);
-			}
-			break;
-		}
-		}
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (showType == ShowType.Queue) {
-			if (currentAnimator != null) {
-				AnimatorStateInfo info = currentAnimator.GetCurrentAnimatorStateInfo (0);
-				if (info.normalizedTime >= 1) {
-					currentIndex++;
-					if(currentIndex == animatorList.Count){
-						currentAnimator = null;				
-					}else{
-						currentAnimator.transform.parent.gameObject.SetActive(false);
-						currentAnimator = animatorList[currentIndex];
-						currentAnimator.transform.parent.gameObject.SetActive(true);
-					}
+		if (currentAnimator != null) {
+			AnimatorStateInfo info = currentAnimator.GetCurrentAnimatorStateInfo (0);
+			if (info.normalizedTime >= 1) {
+				currentIndex++;
+				if(currentIndex == animatorList.Count){
+					currentAnimator = null;				
+				}else{
+					currentAnimator.transform.parent.gameObject.SetActive(false);
+					currentAnimator = animatorList[currentIndex];
+					currentAnimator.transform.parent.gameObject.SetActive(true);
 				}
 			}
 		}
